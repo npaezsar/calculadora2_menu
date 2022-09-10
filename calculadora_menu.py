d5203bb9853c9684682061ab7a6bc3b5a54cd1a2
npaezsar <npaezsar@correo.uis.edu.co>
2022-09-10 08:22:27 -0500

    Se agregó procesamiento en programa

--- calculadora_menu.py
+++ calculadora_menu.py
@@ -22,3 +22,38 @@ print("6. Logaritmo (El logaritmo del primero)")
 
 opcion = int(input("\nDame la opción: "))
 
+# processing
+if(opcion == 1):
+    z = x + y
+    print(x,"+",y)
+elif (opcion == 2):
+    z = x - y
+    print(x,"-",y)
+elif (opcion == 3):
+    z = x * y
+    print(x,"x",y)
+elif (opcion == 4 and y!=0):
+    z = x / y
+    print(x,"/",y)
+elif (opcion == 4 and y==0):
+    print("El denominador es igual a cero y")
+    print("NO se puede realizar la división.")
+    bandera = True
+elif (opcion == 5):
+    z = pow(x,y)
+    print(x,"^",y)
+elif (opcion == 6 and x > 0):
+    z = log(x)
+    print("logaritmo de", x)
+elif (opcion == 6 and x <= 0):
+    print("El valor de x es <= a cero y")
+    print("NO se puede calcular el logaritmo.")
+    bandera = True
+else:
+    print("Opcion no válida")
+
+# se escribe el resutado con otra condición
+if (opcion < 7 and bandera == False):
+    print("Resultado = ", z)
+
+# fin
